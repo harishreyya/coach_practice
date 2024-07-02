419b6291540be8e2c3bac8c0703b18a873bba71d
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2024-07-02 12:04:54 +0530

    todo done using usestate

--- React/usestate_todo/src/App.js
+++ React/usestate_todo/src/App.js
@@ -3,39 +3,54 @@ import './App.css';
 
 function App() {
 const [todos,setTodos] = useState([]);
-const [inputValue,setInputvalue] = useState('');
+const [inputValue,setInputValue] = useState('');
+const [editIndex,setEditIndex]=useState(null);
 
-const addTodo = ()=>{
-    setTodos([...todos,inputValue]);
-    setInputvalue('');
+const addTodo =()=>{
+if(editIndex!==null){
+  const newTodos = [...todos];
+  newTodos[editIndex] = inputValue;
+  setTodos(newTodos);
+  setEditIndex(null)
+}else{
+  setTodos([...todos,inputValue]);
+}
+setInputValue('')
 }
 
-const removeTodo = (index) =>{
- const newTodos = todos.filter((_,i)=>i!==index);
- setTodos(newTodos)
+const removeTodo = (index)=>{
+  const newTodos = todos.filter((_,i)=> i!==index)
+  setTodos(newTodos)
+  
 }
 
-  return (
-    <div className="App">
+const editTodo = (index)=>{
+  setInputValue(todos[index])
+  setEditIndex(index)
+}
 
-      <input placeholder='todo' 
-      value={inputValue} 
-      onChange={(e)=>setInputvalue(e.target.value)}/>
-      <button onClick={addTodo}>submit</button>
 
-      <div>
-        {todos.map((todo,index)=> (
-          <div key={index}>
-          <p>{todo}</p>
-          <button onClick={()=>removeTodo(index)}>remove</button>
-          </div>
-          )
-         
-        )}
+
+return <div>
+  <input placeholder='todo' 
+  value={inputValue}
+  onChange={(e)=>setInputValue(e.target.value)}/>
+  <button onClick={addTodo}>
+    {editIndex!==null?'Update Todo':'Add Todo'}
+  </button>
+
+  {
+    todos.map((todo,index)=>(
+      <div key={index}>
+    <p>{todo}</p>
+    <button onClick={()=>editTodo(index)}>edit</button>
+    <button onClick={()=>removeTodo(index)}>remove</button>
       </div>
-      
-    </div>
-  );
+    ))
+  }
+
+
+</div>
 }
 
 export default App;
